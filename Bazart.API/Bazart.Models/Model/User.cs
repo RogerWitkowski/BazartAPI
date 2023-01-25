6874975844ed128af94752241dd1e9c4ae221f81
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-25 18:15:34 +0100

    Add Identity to solution

--- Bazart.API/Bazart.Models/Model/User.cs
+++ Bazart.API/Bazart.Models/Model/User.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bazart.Models.Model;
+using Microsoft.AspNetCore.Identity;
 
 namespace Bazart.Models.Model
 {
-    internal class User
+    public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Nationality { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public IEnumerable<Event> Events { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
     }
 }

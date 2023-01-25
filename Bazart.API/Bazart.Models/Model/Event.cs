7226330ce5607f9bc8021362dba1f8a286d93f86
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-25 18:46:59 +0100

    Event model implemented

---
+++ Bazart.API/Bazart.Models/Model/Event.cs
@@ -0,0 +1,14 @@
+﻿using Bazart.Models.Model;
+
+namespace Bazart.Models.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public EventDetail EventDetails { get; set; }
+        public string? CreatedById { get; set; }
+        public User CreatedBy { get; set; }
+    }
+}
--- Bazart.API/Bazart.Models/Model/Product.cs
+++ Bazart.API/Bazart.Models/Model/Product.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bazart.Models.Model;
 
 namespace Bazart.Models.Model
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsForSell { get; set; }
+        public string ImageUrl { get; set; }
+        public string CreatedById { get; set; }
+        public User CreatedBy { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }

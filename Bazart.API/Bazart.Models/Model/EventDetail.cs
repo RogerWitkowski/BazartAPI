64fbe5cf517d0a34097956e2cd3ab69a8b8921ac
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-25 19:34:33 +0100

    EventDetail model implemented

---
+++ Bazart.API/Bazart.Models/Model/EventDetail.cs
@@ -0,0 +1,17 @@
+﻿using Bazart.Models.Model;
+
+namespace Bazart.Models.Models
+{
+    public class EventDetail
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseOrFlatNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string ImageUrl { get; set; }
+        public Event Event { get; set; }
+        public int EventId { get; set; }
+    }
+}

936e29d369084c618f506ab45e69f9e5eb1a6462
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-25 19:37:47 +0100

    Address model implemented

---
+++ Bazart.API/Bazart.Models/Model/Address.cs
@@ -0,0 +1,15 @@
+﻿using Bazart.Models.Model;
+
+namespace Bazart.Models.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseOrFlatNumber { get; set; }
+        public string PostalCode { get; set; }
+        public User User { get; set; }
+    }
+}

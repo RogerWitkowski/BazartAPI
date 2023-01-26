c10e7fde78b375086cbac8368f3215289e2ce345
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:41:41 +0100

    Dto created: updateProductDto implemented

---
+++ Bazart.API/Bazart.Models/Dto/ProductDto/UpdateProductDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.ProductDto
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsForSell { get; set; }
+        public string ImageUrl { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+    }
+}

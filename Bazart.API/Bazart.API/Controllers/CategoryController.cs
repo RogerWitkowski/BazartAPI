77cd871c68422addf05db503725dc62ca45c7c0b
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 20:19:28 +0100

    CategoryController implemented

---
+++ Bazart.API/Bazart.API/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Bazart.API.Repository.IRepository;
+using Bazart.Models.Dto.CategoryDto;
+using Bazart.Models.Dto.ProductDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bazart.API.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAll()
+        {
+            var categories = await _categoryRepository.GetAllCategoriesAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{categoryName}")]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetCategoryWithProduct([FromRoute] string categoryName)
+        {
+            var categoryWithProducts = await _categoryRepository.GetCategoriesWithProductsByCategoryNameAsync(categoryName);
+            return Ok(categoryWithProducts);
+        }
+    }
+}

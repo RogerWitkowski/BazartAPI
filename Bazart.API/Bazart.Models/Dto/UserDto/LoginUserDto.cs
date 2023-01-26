d3b8f17b21f5c237a03afccc76c14f4d19eeda62
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:54:47 +0100

    Dto created: loginUserDto implemented

---
+++ Bazart.API/Bazart.Models/Dto/UserDto/LoginUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bazart.Models.Dto.UserDto
+{
+    public class LoginUserDto
+    {
+        [Required]
+        [DataType(dataType: DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(dataType: DataType.Password)]
+        public string Password { get; set; }
+    }
+}

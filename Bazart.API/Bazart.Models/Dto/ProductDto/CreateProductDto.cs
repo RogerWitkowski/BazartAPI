8cfeb91907ea0828a6751b067782d88525f97bc0
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:44:01 +0100

    Dto created: createProductDto implemented

---
+++ Bazart.API/Bazart.Models/Dto/ProductDto/CreateProductDto.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.ProductDto
+{
+    public class CreateProductDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public bool IsForSell { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [MaxLength(19)]
+        public string CategoryName { get; set; }
+    }
+}

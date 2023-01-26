3083d1e6259cc9193d80b0d3d1ff097650bcbd00
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:20:22 +0100

    configuration for AddressModel implemented

---
+++ Bazart.API/Bazart.DataAccess/Configuration/AddressConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Bazart.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bazart.DataAccess.Configuration
+{
+    internal class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> entityTypeBuilder)
+        {
+            entityTypeBuilder
+                .Property(a => a.Country)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(a => a.City)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(a => a.Street)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(a => a.HouseOrFlatNumber)
+                .HasMaxLength(10)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(a => a.PostalCode)
+                .HasMaxLength(10)
+                .IsRequired();
+            entityTypeBuilder
+                .HasOne(u => u.User)
+                .WithOne(a => a.Address)
+                .HasForeignKey<User>(key => key.AddressId);
+        }
+    }
+}

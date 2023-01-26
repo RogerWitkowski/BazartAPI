5f7c9422285c750a12bd96ae94eabd9869a9d775
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:16:14 +0100

    configuration for EventDetailModel implemented

---
+++ Bazart.API/Bazart.DataAccess/Configuration/EventDetailConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Bazart.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bazart.DataAccess.Configuration
+{
+    internal class EventDetailConfiguration : IEntityTypeConfiguration<EventDetail>
+    {
+        public void Configure(EntityTypeBuilder<EventDetail> entityTypeBuilder)
+        {
+            entityTypeBuilder
+                .Property(ed => ed.Country)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(ed => ed.City)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(ed => ed.Street)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(ed => ed.City)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(ed => ed.HouseOrFlatNumber)
+                .HasMaxLength(10)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(ed => ed.PostalCode)
+                .HasMaxLength(10)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(ed => ed.ImageUrl)
+                .IsRequired();
+        }
+    }
+}

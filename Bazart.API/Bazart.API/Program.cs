76df41a9978f0a7348cb01f4caf09ae2b1283dc7
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:24:00 +0100

    more identity options added

--- Bazart.API/Bazart.API/Program.cs
+++ Bazart.API/Bazart.API/Program.cs
@@ -21,6 +21,20 @@ builder.Services.AddScoped<DataGenerator>();
 builder.Services.AddIdentity<User, IdentityRole>(options =>
     {
         options.User.AllowedUserNameCharacters = default;
+
+        options.Password.RequiredLength = 10;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireDigit = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequiredUniqueChars = 2;
+
+        options.User.RequireUniqueEmail = true;
+
+        options.Lockout.MaxFailedAccessAttempts = 3;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+        options.SignIn.RequireConfirmedEmail = true;
     }).AddEntityFrameworkStores<BazartDbContext>()
     .AddDefaultTokenProviders();
 

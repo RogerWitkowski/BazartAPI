9d6b67a9182dda800766186e9b20505d4e287de5
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:08:19 +0100

    configuration for userModel implemented

---
+++ Bazart.API/Bazart.DataAccess/Configuration/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Bazart.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bazart.DataAccess.Configuration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> entityTypeBuilder)
+        {
+            entityTypeBuilder
+                .Property(u => u.FirstName)
+                .HasMaxLength(20)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(u => u.LastName)
+                .HasMaxLength(30)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(u => u.Email)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(u => u.DateOfBirth)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(u => u.Nationality)
+                .IsRequired();
+        }
+    }
+}

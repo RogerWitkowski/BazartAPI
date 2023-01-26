1c6e798ca3131c9c4cb6dbd5bfd8fd7304d23630
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:19:25 +0100

    configuration for CategoryModel implemented

---
+++ Bazart.API/Bazart.DataAccess/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Bazart.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bazart.DataAccess.Configuration
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> entityTypeBuilder)
+        {
+            entityTypeBuilder
+                .Property(c => c.Name)
+                .HasMaxLength(19)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(c => c.Description)
+                .HasMaxLength(50);
+            //    .IsRequired();
+            //entityTypeBuilder
+            //    .Property(c => c.ImageUrl)
+            //    .IsRequired();
+
+            entityTypeBuilder
+                .HasMany(c => c.Products)
+                .WithOne(c => c.Category)
+                .HasForeignKey(key => key.CategoryId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}

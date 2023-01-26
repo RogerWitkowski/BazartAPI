e3be8d5302a2d39ab042b395c74516c05c288888
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:46:55 +0100

    Dto created: eventDto implemented

---
+++ Bazart.API/Bazart.Models/Dto/EventDto/EventDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.EventDto
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseOrFlatNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string ImageUrl { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}

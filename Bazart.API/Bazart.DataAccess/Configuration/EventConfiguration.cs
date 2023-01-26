e0f26c54be44a3ae5c1676781b2e681cde5a1a5e
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:18:07 +0100

    configuration for EventModel implemented

---
+++ Bazart.API/Bazart.DataAccess/Configuration/EventConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Bazart.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bazart.DataAccess.Configuration
+{
+    internal class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> entityTypeBuilder)
+        {
+            entityTypeBuilder
+                .Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+            entityTypeBuilder
+                .Property(e => e.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+            entityTypeBuilder
+                .HasOne(e => e.EventDetails)
+                .WithOne(ed => ed.Event)
+                .HasForeignKey<EventDetail>(key => key.EventId);
+        }
+    }
+}

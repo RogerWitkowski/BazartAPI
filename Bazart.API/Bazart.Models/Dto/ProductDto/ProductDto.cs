eaf0d165492129b90814884ca7d66f8c2abd83b0
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:42:50 +0100

    Dto created: productDto implemented

---
+++ Bazart.API/Bazart.Models/Dto/ProductDto/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.ProductDto
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsForSell { get; set; }
+        public string ImageUrl { get; set; }
+        public string CategoryName { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}

8496d7dace53623a7fabf94ade40356b5a73e943
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:55:39 +0100

    Dto created: registerUserDto implemented

---
+++ Bazart.API/Bazart.Models/Dto/UserDto/RegisterUserDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.UserDto
+{
+    public class RegisterUserDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string ConfirmEmail { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Nationality { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+    }
+}

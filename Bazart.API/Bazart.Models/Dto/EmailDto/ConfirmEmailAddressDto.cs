07bb64e40752737f5c7e67fd56439c47268a6e34
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-26 17:52:51 +0100

    Dto created: confirmEmailAddressDto implemented

---
+++ Bazart.API/Bazart.Models/Dto/EmailDto/ConfirmEmailAddressDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.EmailDto
+{
+    public class ConfirmEmailAddressDto
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+    }
+}

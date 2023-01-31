a66ed9d583f50a58eaad8c14f1021dce7a9c9cba
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:30:16 +0100

    Extension: GlobalErrorHandler - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddGlobalErrorHandlerExtension.cs
@@ -0,0 +1,15 @@
+﻿using Bazart.API.Configurations.Middleware;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    public static class WebApplicationBuilderAddGlobalErrorHandlerExtension
+    {
+        public static WebApplicationBuilder AddGlobalErrorHandlers(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<ErrorHandlingMiddleware>();
+            builder.Services.AddScoped<RequestTimeMiddleware>();
+
+            return builder;
+        }
+    }
+}

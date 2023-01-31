defc1b79b3e2cd5b41630315c29bc42031d6cacc
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:28:21 +0100

    Extension: EmailSendingSettingsModel - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddEmailSendingSettingsModelExtension.cs
@@ -0,0 +1,13 @@
+﻿using Bazart.EmailService.SettingModel;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    internal static class WebApplicationBuilderAddEmailSendingSettingsModelExtension
+    {
+        internal static WebApplicationBuilder AddEmailSendingSettingsModel(this WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<EmailSendingSettingsModel>(builder.Configuration.GetSection("MailGun"));
+            return builder;
+        }
+    }
+}

e991d3ced47ed4bde395b5b92e77f021c0aa7c04
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:27:04 +0100

    Extension: DataGenerator - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddDataGeneratorExtension.cs
@@ -0,0 +1,13 @@
+﻿using Bazart.DataAccess.Seeder;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    public static class WebApplicationBuilderAddDataGeneratorExtension
+    {
+        public static WebApplicationBuilder AddDataGenerator(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<DataGenerator>();
+            return builder;
+        }
+    }
+}

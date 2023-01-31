6213695416d7b03cf76c59d91a6c46b0baea9c05
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:38:26 +0100

    Extension: use dataGenerator - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Use/WebApplicationUseDataGeneratorExtension.cs
@@ -0,0 +1,16 @@
+﻿using Bazart.DataAccess.Seeder;
+
+namespace Bazart.API.Configurations.Extensions.Use
+{
+    internal static class WebApplicationUseDataGeneratorExtension
+    {
+        internal static async Task<WebApplication> UseDataGenerator(this WebApplication app)
+        {
+            await using var scope = app.Services.CreateAsyncScope();
+            var dataGenerator = scope.ServiceProvider.GetService<DataGenerator>();
+            await dataGenerator!.GenerateData();
+
+            return app;
+        }
+    }
+}

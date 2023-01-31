cf63faf34c176e4b39806b49e7d2c1e870089d01
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:37:40 +0100

    Extension: use CorsPolicy - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Use/WebApplicationUseCorsPolicyExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Bazart.API.Configurations.Extensions.Use
+{
+    public static class WebApplicationUseCorsPolicyExtension
+    {
+        public static WebApplication UseCorsPolicy(this WebApplication app)
+        {
+            app.UseCors(policyName: "FrontendClient");
+
+            return app;
+        }
+    }
+}

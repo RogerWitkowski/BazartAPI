e2dbbf6783ef65acc6f8b89d5780999088854f78
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:39:08 +0100

    Extension: use globalErrorHandlerMiddlewar - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Use/WebApplicationUseGlobalErrorHandlerMiddlewareExtension.cs
@@ -0,0 +1,14 @@
+﻿using Bazart.API.Configurations.Middleware;
+
+namespace Bazart.API.Configurations.Extensions.Use
+{
+    public static class WebApplicationUseGlobalErrorHandlerMiddlewareExtension
+    {
+        internal static WebApplication UseGlobalErrorHandlersMiddleware(this WebApplication app)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.UseMiddleware<RequestTimeMiddleware>();
+            return app;
+        }
+    }
+}

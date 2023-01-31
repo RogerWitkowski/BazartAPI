0e7c50176c18e4c0f0b5956067c71159f97e2cd5
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:24:55 +0100

    Extension: ControllersWithJsonOptions - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddControllersWithJsonOptionsExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    internal static class WebApplicationBuilderAddControllersWithJsonOptionsExtension
+    {
+        internal static WebApplicationBuilder AddControllersWithJsonOptions(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                options.JsonSerializerOptions.WriteIndented = true;
+            }).AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddXmlDataContractSerializerFormatters();
+
+            return builder;
+        }
+    }
+}

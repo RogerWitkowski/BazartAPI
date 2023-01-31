d93e2fb55e307425638fc9e5c5f65102b5fd69fc
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:29:16 +0100

    Extension: EmailService - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddEmailServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using Bazart.EmailService.EmailService.IEmailService;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    internal static class WebApplicationBuilderAddEmailServiceExtension
+    {
+        internal static WebApplicationBuilder AddEmailService(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<IEmailService, EmailService.EmailService.EmailService>();
+            return builder;
+        }
+    }
+}

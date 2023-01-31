69b9fd903b3795a87593613ec8577b75a5ebad07
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:22:54 +0100

    Extension: autoMapper - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddAutoMapperExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    public static class WebApplicationBuilderAddAutoMapperExtension
+    {
+        public static WebApplicationBuilder AddAutoMapper(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return builder;
+        }
+    }
+}

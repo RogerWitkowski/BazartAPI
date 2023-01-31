934e6dd864523bad84f750ec37a6e5a431b846ea
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:27:36 +0100

    Extension: DbContext - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddDbContextExtension.cs
@@ -0,0 +1,17 @@
+﻿using Bazart.DataAccess.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    public static class WebApplicationBuilderAddDbContextExtension
+    {
+        public static WebApplicationBuilder AddDbContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<BazartDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"));
+            });
+            return builder;
+        }
+    }
+}

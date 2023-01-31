2715b40d87eba3bbd0c76031ab1b0dc145990b04
RogerWitkowski <witkowski.roger@gmail.com>
2023-01-31 22:25:43 +0100

    Extension: CorsPolicy - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddCorsPolicyExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Bazart.API.Configurations.Extensions.Add
+{
+    internal static class WebApplicationBuilderAddCorsPolicyExtension
+    {
+        internal static WebApplicationBuilder AddCorsPolicy(this WebApplicationBuilder builder)
+        {
+            var allowedOrigin = builder.Configuration.GetSection("AllowedOrigins:ReactFrontOrigin").Value;
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("FrontendClient", policy => policy
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins(origins: allowedOrigin)
+                    .AllowCredentials());
+            });
+            return builder;
+        }
+    }
+}

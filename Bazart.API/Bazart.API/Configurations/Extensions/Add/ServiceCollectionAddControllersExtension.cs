3597c1113a21e0334aff23b486a8f16039047288
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-01 16:53:31 +0100

    Extension: Controllers - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/ServiceCollectionAddControllersExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Bazart.API.Configurations.Extensions.Add
+{
+    public static class ServiceCollectionAddControllersExtension
+    {
+        public static IServiceCollection AddControllersCollection(this IServiceCollection service)
+        {
+            var assembly = typeof(Bazart.Controllers.Controllers.AccountController).Assembly;
+            service.AddMvc().AddApplicationPart(assembly);
+            return service;
+        }
+    }
+}

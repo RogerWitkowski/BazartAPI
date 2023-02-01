7fb9ed5f82858548e4cad0cf9b6c123fb7b565c0
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-01 16:54:24 +0100

    Extension: Repositories - implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/ServiceCollectionAddRepositoriesExtension.cs
@@ -0,0 +1,19 @@
+﻿using Bazart.Repository.Repository;
+using Bazart.Repository.Repository.IRepository;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    public static class ServiceCollectionAddRepositoriesExtension
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IItemsRepository, ItemsRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+
+            return services;
+        }
+    }
+}

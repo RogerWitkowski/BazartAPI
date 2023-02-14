03feab48edcde5346bb82da565860ff85ec42b4e
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-14 16:48:20 +0100

    cors policy extension bug fixed

--- Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddCorsPolicyExtension.cs
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddCorsPolicyExtension.cs
@@ -1,18 +1,27 @@
-﻿namespace Bazart.API.Configurations.Extensions.Add
+﻿using Bazart.ErrorHandlingMiddleware.Exceptions;
+using static System.Net.WebRequestMethods;
+
+namespace Bazart.API.Configurations.Extensions.Add
 {
     public static class WebApplicationBuilderAddCorsPolicyExtension
     {
         public static WebApplicationBuilder AddCorsPolicy(this WebApplicationBuilder builder)
         {
             var allowedOrigin = builder.Configuration.GetSection("AllowedOrigins:ReactFrontOrigin").Value;
+            if (string.IsNullOrEmpty(allowedOrigin))
+            {
+                throw new NotFoundException("Origin not found.");
+            }
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("FrontendClient", policy => policy
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                //.WithOrigins(origins: allowedOrigin)
-                //.AllowCredentials()
-                .AllowAnyOrigin()
+
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        //policy.WithOrigins(origins: allowedOrigin);
+                        //policy.AllowCredentials();
+                        .AllowAnyOrigin()
+
                 );
             });
             return builder;

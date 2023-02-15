e731f2cebe1337050efbfddc6eff5b32b16fd22a
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-15 21:24:52 +0100

    jwtTokenCreate method implemented; add role to register user

--- Bazart.API/Bazart.Repository/Repository/AccountRepository.cs
+++ Bazart.API/Bazart.Repository/Repository/AccountRepository.cs
@@ -1,11 +1,19 @@
-﻿using AutoMapper;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AutoMapper;
+using Bazart.AuthenticationSettings.Settings;
+using Bazart.DataAccess.DataAccess;
 using Bazart.ErrorHandlingMiddleware.Exceptions;
 using Bazart.Models.Dto.EmailDto;
 using Bazart.Models.Dto.UserDto;
 using Bazart.Models.Model;
 using Bazart.Repository.Repository.IRepository;
+using Bazart.Utils;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Bazart.Repository.Repository
 {
@@ -14,12 +22,16 @@ namespace Bazart.Repository.Repository
         private readonly IMapper _mapper;
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly AuthenticationSettingsModel _authenticationSettings;
+        private readonly BazartDbContext _dbContext;
 
-        public AccountRepository(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountRepository(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager, AuthenticationSettingsModel authenticationSettings, BazartDbContext dbContext)
         {
             _mapper = mapper;
             _userManager = userManager;
             _signInManager = signInManager;
+            _authenticationSettings = authenticationSettings;
+            _dbContext = dbContext;
         }
 
         public async Task<ActionResult<ConfirmEmailAddressDto>> RegisterUserAsync(RegisterUserDto registerUserDto)
@@ -32,6 +44,7 @@ namespace Bazart.Repository.Repository
 
                 if (createdUser.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(newUser, Roles.Customer);
                     var token = await GenerateConfirmationToken(newUser);
                     return token;
                 }
@@ -46,7 +59,7 @@ namespace Bazart.Repository.Repository
 
             if (result.Succeeded)
             {
-                return new NoContentResult();
+                return new OkObjectResult("ok");
             }
             else
             {
@@ -60,8 +73,8 @@ namespace Bazart.Repository.Repository
                 var isUserExist = await _userManager.FindByEmailAsync(loginUserDto.Email);
                 if (isUserExist != null)
                 {
-                    var token = await GenerateConfirmationToken(isUserExist);
-                    return token;
+                    var token = GenerateConfirmationToken(isUserExist);
+                    return token.Result;
                 }
             }
 
@@ -95,6 +108,55 @@ namespace Bazart.Repository.Repository
         {
             var confirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
             return new ConfirmEmailAddressDto() { Token = confirmationToken, UserId = user.Id };
+        }
+
+        public async Task AuthWithMFA()
+        {
+            //User user = await _userManager.GetUserAsync()
+            //_userManager.GetAuthenticatorKeyAsync()
+            throw new Exception("fdfs");
+        }
+
+        public async Task<string> CreateJwtToken(LoginUserDto loginUserDto)
+        {
+            var user = await _userManager
+                .Users
+                .FirstOrDefaultAsync(u => u.Email == loginUserDto.Email);
+
+            var userRole = await _dbContext.UserRoles.FirstOrDefaultAsync(u => u.UserId == user.Id);
+
+            if (user == null)
+            {
+                throw new BadRequestException("Invalid email or password.");
+            }
+
+            var isPwCorrect = _userManager.PasswordHasher.VerifyHashedPassword(user, user.PasswordHash, loginUserDto.Password);
+
+            if (isPwCorrect == PasswordVerificationResult.Failed)
+            {
+                throw new BadRequestException("Invalid email or password.");
+            }
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
+                new Claim(ClaimTypes.Role, $"{userRole.RoleId}"),
+                new Claim(ClaimTypes.DateOfBirth, user.DateOfBirth.Value.ToString("yyyy-MM-dd")),
+                new Claim("Nationality", user.Nationality)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiresAt = DateTime.Now.AddMinutes(_authenticationSettings.JwtExpireMinutes);
+
+            var token = new JwtSecurityToken(_authenticationSettings.JwtIssuer, _authenticationSettings.JwtAudience,
+                claims,
+                expires: expiresAt,
+                signingCredentials: credentials);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
         }
     }
 }

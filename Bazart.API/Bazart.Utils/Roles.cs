425d2ece705c99fe42ada0ace4c3a393a0d44247
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-16 21:25:55 +0100

    Roles static class implemented

---
+++ Bazart.API/Bazart.Utils/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace Bazart.Utils
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string Artist = "Artist";
+    }
+}

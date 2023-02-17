c28c802a47246fd17ee108cc8ad3600551afd016
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-17 19:39:20 +0100

    authenticationSettings extension implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddAuthenticationSettingsExtension.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Bazart.AuthenticationSettings.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Bazart.API.Configurations.Extensions.Add
+{
+    internal static class WebApplicationBuilderAddAuthenticationSettingsExtension
+    {
+        internal static WebApplicationBuilder AddAuthenticationSettings(this WebApplicationBuilder builder)
+        {
+            var authenticationSettings = new AuthenticationSettingsModel();
+
+            builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+            builder.Services.AddSingleton(authenticationSettings);
+
+            builder.Services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(config =>
+            {
+                config.RequireHttpsMetadata = false;
+                config.SaveToken = true;
+                config.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = authenticationSettings.JwtIssuer,
+                    ValidAudience = authenticationSettings.JwtAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey))
+                };
+            });
+            return builder;
+        }
+    }
+}

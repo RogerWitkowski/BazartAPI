ab7a7d6dc2a177b3dcfd78d19664e7fc8a2a9e30
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-17 19:36:23 +0100

    jwtAuthenticationsettings model implemented

---
+++ Bazart.API/AuthenticationSettings/Settings/AuthenticationSettingsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Bazart.AuthenticationSettings.Settings
+{
+    public class AuthenticationSettingsModel
+    {
+        public string JwtKey { get; set; }
+        public int JwtExpireMinutes { get; set; }
+        public string JwtIssuer { get; set; }
+        public string JwtAudience { get; set; }
+    }
+}

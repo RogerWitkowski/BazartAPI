d0707b55f243656da7374d8431eee402b8b19c7c
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-17 19:40:16 +0100

    authorizationPolicy extension implemented

---
+++ Bazart.API/Bazart.API/Configurations/Extensions/Add/WebApplicationBuilderAddAuthorizationPolicyExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Bazart.API.Configurations.Extensions.Add
+{
+    public static class WebApplicationBuilderAddAuthorizationPolicyExtension
+    {
+        public static WebApplicationBuilder AddAuthorizationPolicy(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("NormalUser", config => config.RequireClaim("User"));
+            });
+            return builder;
+        }
+    }
+}

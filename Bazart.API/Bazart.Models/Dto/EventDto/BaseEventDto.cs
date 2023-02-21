82c60be4f685aea88e26692b73f676d5669972c9
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-21 20:26:53 +0100

    BaseEventDto model created

---
+++ Bazart.API/Bazart.Models/Dto/EventDto/BaseEventDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.EventDto
+{
+    public abstract class BaseEventDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseOrFlatNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

56406c23c777e8ee1cd85e95ded5dbf540d64a88
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-21 20:45:33 +0100

    BaseProductDto created

---
+++ Bazart.API/Bazart.Models/Dto/ProductDto/BaseProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models.Dto.ProductDto
+{
+    public abstract class BaseProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsForSell { get; set; }
+        public string ImageUrl { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
